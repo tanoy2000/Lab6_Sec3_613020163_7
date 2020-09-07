166af8d8c966df399612360637078b74f3bad803
tanoy2000 <pantita.ma@kkumail.com>
2020-09-07 18:28:26 +0700

    Lab6_Sec3_613020163_7

---
+++ app/src/main/java/com/example/lab6/TwoFragment.kt
@@ -0,0 +1,26 @@
+package com.example.lab6
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_three.view.*
+import kotlinx.android.synthetic.main.fragment_two.view.*
+
+
+class TwoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_two, container, false)
+        view.btnClickFragTwo.setOnClickListener(){
+            val toast = Toast.makeText(context, "Click On Fragment Three", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        return view
+    }
+}
